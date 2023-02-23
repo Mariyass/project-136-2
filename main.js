3e4714270dbcd975a264754a99ad117e60242b81
Mariyass <77000572+Mariyass@users.noreply.github.com>
2023-02-23 16:34:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+var status;
+var video="";
+var value="";
+
+
+function setup(){
+    canvas=createCanvas(480,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+
+function start(){
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects"
+    document.getElementById("input").value
+
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status=true;
+}
+
+function draw(){
+    image(video,0,0,480,380);
+}
